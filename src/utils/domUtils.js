6f14a0136f20df914f243cd5eae0e2bfe2d1416f
mebius <591626940@qq.com>
2025-10-06 20:53:07 +0800

    docs(domUtils): 添加函数注释以提高代码可读性

--- src/utils/domUtils.js
+++ src/utils/domUtils.js
@@ -1,9 +1,16 @@
-// @ts-nocheck
+// 是否为事件属性
 export const isEvent = (key) => key.startsWith("on");
+
+// 是否为DOM属性
 export const isProperty = (key) => key !== "children" && !isEvent(key);
+
+// 提取事件类型
 export const eventType = (key) => key.substring(2).toLowerCase();
 
+// 属性是否被移除
 export const isGone = (prev, next) => (key) => !(key in next);
+
+// 属性是否为新值
 export const isNew = (prev, next) => (key) => prev[key] !== next[key];
 
 
